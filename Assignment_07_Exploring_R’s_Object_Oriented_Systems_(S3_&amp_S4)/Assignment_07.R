29f588184eadbef89f5bc4f627bc03111a9ccf1f
HaoZhong15 <97054618+HaoZhong15@users.noreply.github.com>
2025-10-12 22:27:03 -0400

    Assignment_07.R

--- Assignment_07_Exploring_R’s_Object_Oriented_Systems_(S3_&amp_S4)/Assignment_07.R
+++ Assignment_07_Exploring_R’s_Object_Oriented_Systems_(S3_&amp_S4)/Assignment_07.R
@@ -1,1 +1,63 @@
+# 1. Choose or Download Data
 
+
+data("mtcars")
+
+head(mtcars)
+
+str(mtcars)
+
+
+
+# 2. Test Generic Functions
+summary(mtcars)
+
+print(mtcars)
+
+
+
+# 3. Explore S3 vs. S4
+# s3
+
+car <- mtcars[3, , drop = FALSE]
+
+car_s3 <- list(
+  model = rownames(car),
+  cyl   = as.numeric(car$cyl),
+  hp   = as.numeric(car$hp),
+  mpg    = as.numeric(car$mpg)
+)
+class(car_s3) <- "car_s3"
+
+
+print.car_s3 <- function(x, ...) {
+  cat("S3 Car\n")
+  cat(" model:", x$model, "\n")
+  cat(" cyl  :", x$cyl,   "\n")
+  cat(" hp  :", x$hp,   "\n")
+  cat(" mpg   :", x$mpg,    "\n")
+}
+
+print(car_s3)
+
+
+# s4
+
+setClass("car_s4", slots = c(model = "character", cyl   = "numeric", mpg   = "numeric", hp    = "numeric"))
+
+car_s4 <- new("car_s4", 
+              model = rownames(car),
+              cyl   = as.numeric(car$cyl),
+              hp   = as.numeric(car$hp),
+              mpg    = as.numeric(car$mpg))
+
+
+setMethod("show", "car_s4", function(object) {
+  cat("S4 Car\n")
+  cat(" model:", object@model, "\n")
+  cat(" cyl  :", object@cyl,   "\n")
+  cat(" hp  :", object@hp,   "\n")
+  cat(" mpg   :", object@mpg,    "\n")
+})
+
+car_s4
