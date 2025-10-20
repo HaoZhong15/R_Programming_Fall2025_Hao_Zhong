1e349cea588293648bf5761d73a4a7726375de02
HaoZhong15 <97054618+HaoZhong15@users.noreply.github.com>
2025-10-19 22:34:15 -0400

    Assignment_08.R

--- Assignment_08_input_Output_String_Manipulation_and_the_plyr_Package/Assignment_08.R
+++ Assignment_08_input_Output_String_Manipulation_and_the_plyr_Package/Assignment_08.R
@@ -1,1 +1,21 @@
+# 1. Import Assignment 6 Data
 
+student6 <- read.table(file.choose(), header = TRUE, stringsAsFactors = FALSE)
+head(student6)
+
+
+library(plyr)
+
+gender_mean <- ddply(student6, "Sex", summarise, GradeAverage = mean(Grade, na.rm = TRUE))
+gender_mean
+
+write.table(gender_mean, file = "gender_mean.txt", sep  = "\t", row.names = FALSE)
+getwd()
+
+i_students <- subset(student6, grepl("i", Name, ignore.case = TRUE))
+i_students
+
+write.csv(i_students$Name, file = "i_students.csv", row.names = FALSE, quote = FALSE)
+
+write.csv(i_students, file = "i_students_full.csv", row.names = FALSE)
+getwd()
